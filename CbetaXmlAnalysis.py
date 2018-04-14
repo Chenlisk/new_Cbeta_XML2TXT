ff0c0d91ea318e1b9240b852d80734fbf23c0adf
ShiXianjiong <chenlisk@outlook.com>
2018-04-14 14:53:51 +0800

    V0.98
    2018-4-14 14:50

--- CbetaXmlAnalysis.py
+++ CbetaXmlAnalysis.py
@@ -1,79 +1,131 @@
-###------------------------------------------------------------------------------------------------###
+##---------------------------------------------------------------------------------------###
 #Cbeta Xml-p5
 #Data Source:https://github.com/cbeta-org/xml-p5
 #
-###------------------------------------------------------------------------------------------------###
+###--------------------------------------------------------------------------------------###
 import datetime
 import os
 import re
 import sys
 import time
+#-------------------------------------------------------------------------------------------
+DEBUG=False
+Write = True
+Path='xml-p5-master'
+puncPattern=r'(\w|[　❥【】《》〔〕（）]|<.{0,5}>)'
+#-------------------------------------------------------------------------------------------
 
-def main(base=0):
-    path='xml-p5-master\\T'
+def main(base=0,lens=1):   
     fileList = []
-    for dirpath, dirnames, filenames in os.walk(path):
+    for dirpath, dirnames, filenames in os.walk(Path):
         for file in filenames:
             if file.endswith('.xml'):
                  fileList.append(os.path.join(dirpath, file))
 
     top=len(fileList)
-    top=base+15
+
+    if DEBUG:
+        top=base+lens
 
     for i in range(base,top):
         data=readFile(fileList[i])
         process(data,fileList[i])
         print(str(i+1).rjust(3,'0'),'/',len(fileList),'---$:',fileList[i])
 
-#==========================================================    
+#==========================================================  
 def preProcess(data):
     data=getStr(data,'<body>','</body>')
     data=data.replace('\n','')
-    data=re.sub(r'	','',data)
+    data=re.sub(r'	','',data) 
 
-    data=re.sub(r'<cb:docNumber>.+?</cb:docNumber>','',data)
+    data=re.sub(r'<!--.+?-->','',data)
+    data=re.sub(r'<cb:docNumber>(.|\n)+?</cb:docNumber>','',data)
     data=re.sub(r'<anchor xml.+?/>','',data)
-    data=re.sub(r'<byline cb.+?</byline>','',data)
-    data=re.sub(r'<head>.+?</head>','',data)
+    data=re.sub(r'<byline.+?</byline>','',data)
+    data=re.sub(r'<head.+?</head>','',data)
+   
+    data=re.sub(r'<cb:div type="pin">','\n<pin>\n',data)
+    data=re.sub(r'<cb:div type="w">(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="other">.+?>題解<(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="dharani">(.|\n)+?</cb:div>','\n\n',data)#</cb:div>
+    data=re.sub(r'<cb:div.+?type=.+?>','\n\n',data)#</cb:div> 
+       
+    data=re.sub(r'<cb:div type="pin">','\n<pin>\n',data)
+    data=re.sub(r'<cb:div type="w">(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="other">.+?>題解<(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="dharani">(.|\n)+?</cb:div>','\n\n',data)#</cb:div>
+    data=re.sub(r'<cb:div.+?type=.+?>','\n\n',data)#</cb:div> 
+    data=re.sub(r'<cb:juan.+?fun="open".+?</cb:juan>','\n<juan>\n',data)
     data=re.sub(r'<cb:juan.+?</cb:juan>','',data)
-    data=re.sub(r'<cb:mulu.+?</cb:mulu>','',data)
+    data=re.sub(r'<cb:mulu.+?</cb:mulu>','',data)  
+    data=re.sub(r'(<cb:dialog type(.|\n)+?>|</cb:dialog>)','',data)
+    
     data=re.sub(r'<anchor type="circle"/>','',data)
+    data=re.sub(r'<table(.|\n)+?</table>','',data)
+    data=re.sub(r'<date>(.|\n)+?</date>','',data)
+    data=re.sub(r'<formula(.|\n)+?</formula>','',data)
+
+    data=re.sub(r'(<sp cb:type(.|\n)+?>|</sp>)','',data)
+    data=re.sub(r'</?cb:event>','',data)
+    data=re.sub(r'<unclear></unclear>','',data)
+
+    data=re.sub(r'(<list>|</list>)','',data)
+    data=re.sub(r'(<item xml.+?>|</item>)','',data)
+    data=re.sub(r'<lb.+?ed=.+?/>','',data)
+
+    data=fullyDel('<entry','</entry>',data)
+   
+    data=re.sub(r'<cb:div type="pin">','\n<pin>\n',data)
+    data=re.sub(r'<cb:div type="w">(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="other">.+?>題解<(.|\n)+?</cb:div>','',data) 
+    data=re.sub(r'<cb:div type="dharani">(.|\n)+?</cb:div>','\n\n',data)#</cb:div>
+    data=re.sub(r'<cb:div.+?type=.+?>','\n\n',data)#</cb:div> 
     #--------------------------------------------------------------------------
-    data=re.sub(r'<cb:div type=.+?>','\n\n',data)#</cb:div>
+    data=re.sub(r'<p cb:type="head.+?>','',data) 
+    data=re.sub(r'<p cb:type="pre.+?</p>','',data) 
     data=re.sub(r'<p xml.+?>','<p>',data)
     data=re.sub(r'<lb.+?ed=.+?/>','',data)
     data=re.sub(r'<pb (xml:id=.+?)?(n=.+?)?ed=.+?/>','',data)
-    data=re.sub(r'(<lg (xml:id=|rend).+?>|</lg>)','',data)
+    data=re.sub(r'(<lg (xml:id=|rend|type=).+?>|</lg>)','',data)
     data=re.sub(r'<note place="inline">.+?</note>','',data)
-    data=re.sub(r'<cb:tt>.+?</cb:tt>','',data)  
+    data=re.sub(r'<ref target="#PTS(.|\n)+?</ref>','',data)
+    data=re.sub(r'<label type=(.|\n)+?</label>','',data)
+
     data=re.sub(r'<head.+?</head>','',data)  
     data=re.sub(r'<milestone.+?(unit="juan")?/>','',data)
-    data=re.sub(r'</cb:div>','',data)       
+    data=re.sub(r'</?cb:div>','',data)       
     data=re.sub(r'<space quantity.+?/>','',data)
-    data=re.sub(r'<l rend="text-indent.+?>','<l>',data) 
+    data=re.sub(r'<l rend=".+?>','',data) 
+    data=re.sub(r'</?l>','',data)
+    data=re.sub(r'<figure>.+?</figure>','',data) 
+    #--------------------------------------------------------------------------
+    data=re.sub(r'<trailer>(.|\n)+?</trailer>','',data) 
+    data=re.sub(r'[△○﹂]','',data) 
 
-    # data=re.sub(r'</l><p>','</p>\n<p>',data)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-    # data=re.sub(r'</p><l>','',data)      
-    data=re.sub(r'(</l><l>)','',data)
-    # data=re.sub(r'(</l>|<l>)','',data)
+    data=re.sub(r'<cb:yin>.+?/cb:yin>','',data) 
+    data=re.sub(r'(</?cb:fan>|</?cb:zi>)','',data)
 
-    # data=re.sub(r'</p><p>','</p>\n<p>',data) 
-    # data=re.sub(r'</p><p cb:type','</p>\n<p cb:type',data) 
-    # data=re.sub(r'：</p>\n?<p>','：',data) 
-    
-    #    data=re.sub(r'','',data) 
-    #    data=re.sub(r'','',data) 
-    #    data=re.sub(r'','',data) 
-    #    data=re.sub(r'','',data) 
-    #    data=re.sub(r'','',data) 
-    #    data=data.replace('</l><p>','</p>\n<p>')
-    #    data=data.replace('</p><l>','')
+    data=re.sub(r'<cb:tt>.+?</cb:tt>','',data)  
+    data=re.sub(r'<cb:t xml:lang="sa-Sidd">.+?/cb:t>','',data) 
+    data=re.sub(r'(</?cb:tt?>|<cb:tt? .+?>)','',data) 
+    data=re.sub(r'(<term xml:lang="sa\-Sidd">|</term>)','',data)
+
     return data
 
+
 def process(data,path):
-    tdata=re.sub(r'(\W|[0-z])','',data)
-    addr='Result'+path[path.rfind('\\'):].replace('.xml','.txt')
-    writeFile(addr,tdata)
+    path='Result'+path[path.find('\\'):]
+    path=re.sub(r'[A-Z]{1,2}\d{2,3}\\','',path)
+
+    if DEBUG and Write:
+        tdata=re.sub(r'(\W|[0-z])','',data)
+        addr=path.replace('.xml','-T.xml')
+        writeFile(addr,tdata)
+
+    if DEBUG and Write:
+        bdata=getStr(data,'<body>','</body>')
+        addr=path.replace('.xml','-B.xml')
+        writeFile(addr,bdata)
 
     mlist=createMap(data)
     data=preProcess(data)
@@ -81,19 +133,104 @@ def process(data,path):
     data=dharani(data)
     data=lastProcess(data)
 
-    addr='Result'+path[path.rfind('\\'):]
-    writeFile(addr,data)
+    if DEBUG and Write:
+        addr=path.replace('.xml','-D.xml')
+        writeFile(addr,data)
+
+    if DEBUG and Write:
+        pdata=re.sub(puncPattern,'',data)
+        addr=path.replace('.xml','-P.xml')
+        writeFile(addr,pdata)  
+
+    data=checkPunc(data)
+    if Write:        
+        addr=path.replace('.xml','_out.xml')
+        writeFile(addr,data)
+    
+    if DEBUG:
+        ldata=re.sub(r'(<p>|<pin>|<juan>)','',data)
+        pattern=re.compile(r'<.+?>')
+        lis=pattern.findall(ldata)
+        if len(lis)>0:
+            print(lis[0])
+    
+    output(data,path)
+
+def output(dat,path):
+    dat=re.sub(r'(<p>|<pin>|\n)','',dat)
+    dat=re.sub(r'<juan>','\n\n',dat)
+    dat=dat.lstrip('\n').rstrip('\n')
+    addr=path.replace('.xml','.out').replace('Result','TrainData')
+    writeFile(addr,dat)     
 
 def lastProcess(data):
-    data=re.sub(r'\n<p></p>\n+','',data) 
+    data=re.sub(r'(</p>)','',data)
+    data=re.sub(r'<p>','\n<p>',data)
+    data=re.sub(r'<pin>','\n<pin>\n',data)
+    data=re.sub(r'<juan>','\n<juan>\n',data)
+    data=data.lstrip('\n').rstrip('\n')
+    data=re.sub(r'：\n<p>','：',data)
+    data=re.sub(r'\n+','\n',data)
 
-    data=re.sub(r'　','',data)
-
-
-    # data=re.sub(r'<p>.+?</p>\n?','',data)#---------------------------------------test
-    data=re.sub(r'\n\n+','\n\n',data)   
+    data=re.sub(r'(<.+?>)','',data)    
 
     return data
+
+def checkPunc(data):
+    # data=data.replace('<','#<')
+    sdata=data.split('\n')
+    for i in range(len(sdata)):
+        s=sdata[i]
+        if s=='<pin>' or s=='<juan>':
+            continue
+
+        if len(s)>500 and s.count('。')<=1: 
+            s=''
+        
+        if  len(s)<=500 and s.count('。')<=1 and s.count('？')==0 and s.count('，')==0 and s.count('！')==0 and s.count('：')==0 and s.count('、')==0 and s.count('；')==0: 
+            s=''
+        
+        es=re.sub(puncPattern,'',s)
+        
+        if len(es)!=0:
+            # print(es.count('。'),len(es)  ,es.count('。')/len(es))
+            if len(es)<2:
+                s=''
+            if es.count('。')/len(es)>0.9 and len(es)>3:
+                s=''
+
+            jd= es.replace('。','').replace('．','')
+            if len(jd)==0:
+                s=''
+
+        #--------------------------------------------------------------
+        s=re.sub(r'□…□.+?。','',s)
+        s=re.sub(r'.+…….+','',s)
+        #--------------------------------------------------------------
+
+        if s.count('「')==1 and s[3]=='「':
+            s=s.replace('「','')
+            if s[-1]=='」':
+                s=s.replace('」','')
+
+        if s.count('『')==1 and s[3]=='『':
+            s=s.replace('『','')
+            if s[-1]=='』':
+                s=s.replace('』','')
+
+        if s!='' and s.endswith('。')==False and s.endswith('！')==False and s.endswith('？')==False and s.endswith('」')==False and s.endswith('』')==False:
+            s+='。'
+
+        sdata[i]=s
+    data=''.join(sdata)
+    data=re.sub(r'<','\n<',data)
+    data=re.sub(r'<pin>\n+<juan>','',data)
+    data=re.sub(r'<pin>\n+<pin>','',data)
+    data=re.sub(r'<juan>\n+<juan>','',data)
+    data=re.sub(r'\n+','\n',data)
+    data=data.lstrip('\n').rstrip('\n')
+    return data
+    
 
 def createMap(data):
     mlist=[]
@@ -128,13 +265,24 @@ def replaceCB(data,mlis):
 def dharani(data):
     while data.find('<p cb:type="dharani" xml:id=')!=-1:
         d=getStr(data,'<p cb:type="dharani" xml:id=','</p>','w')
-        r=d.replace('　','，')
-        r=r.replace('』</p>','。』</p>')
-        r=re.sub(r'<p cb:type="dharani" xml:id=.+?>','<p>',r)
+        # r=d.replace('　','，')
+        # r=r.replace('』','。』')
+        # r=r.replace('</p>','。</p>').replace('。。','。')
+        r=re.sub(r'<p cb:type="dharani" xml:id=.+?>','<p>',d)
         data=data.replace(d,r)
     return data
 
 #==========================================================
+def fullyDel(head,tial,data):
+    while data.find(head)!=-1:
+        h=data.rfind(head)
+        hdata=data[0:h]
+        tdata=data[h:]
+        t=tdata.find(tial)+len(tial)
+        tdata=tdata[t:]
+        data=hdata+tdata
+    return data
+
 def getStr(data,head,tial,width='n'):
     if data.find(head)==-1 or data.find(head) ==-1:
         return ''
@@ -163,11 +311,19 @@ def writeFile(filedir, string):
     with open(filedir, "w+", encoding='utf-8') as f:
         f.write(string)
 #==========================================================
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
+if __name__ == '__main__':    
+    x=1
+    y=1
+    if len(sys.argv) > 2:       
+        y=sys.argv[2]
+        y = int(y)
         x = sys.argv[1]
-        x = int(x)
-        main(x-1)
+        x = int(x)-1    
+        main(x,y) 
+    elif len(sys.argv) > 1: 
+        x = sys.argv[1]
+        x = int(x)-1 
+        main(x,y)       
     else:
-        main()    
+        main() 
 #==========================================================
